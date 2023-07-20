7a1697c62cd82fd17e8310113157f64f057f8d4c
petronije2002 <petronije2002@gmail.com>
2023-07-20 12:18:37 +0200

    before sinusoidal PWM

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,13 @@
 #include <SPI.h>
 // #include <Serial.h>
 #include <PID_v1.h>
+#include <SimpleFOC.h>
+
+
+
+
+
+
 
 double setpoint = 100.0; // Target position
 double input = 0.0;      // Current position
@@ -20,15 +27,21 @@ double output = 0.0;     // Control output
 #define INH_C_PIN 25
 #define INL_C_PIN 33
 
+
+
+
+
+
+
 #define PHASE_DELAY_1 (double)2.094395102 // 120°
 #define PHASE_DELAY_2 (double)4.188790205 // 240°
 
-const float MAX_POSITION = 359.9;
+const float MAX_POSITION = 359.9 * PI / 180;
 const float MIN_POSITION = 0.0;
 
 float direction = 0;
 
-float positionIncrement = 0.0001;
+float positionIncrement = 0.001;
 
 float actingAngle = 0;
 
@@ -39,7 +52,7 @@ int POLE_PAIRS = 21;
 
 float PHASE_SHIFT = 1;
 
-float OFFSET = 0.00;
+float OFFSET = -0.4;
 
 float smallMovement = 0;
 
@@ -47,8 +60,6 @@ unsigned long currentTime;
 unsigned long elapsedTime;
 double integralSum = 0.0;
 const double integralWindupMax = 3.0; // Maximum value for integral sum (adjust as needed)
-
-
 
 float dutyCycle = 0;
 float mappedControl;
@@ -64,7 +75,7 @@ float currentVelocity = 0.0;  // Current velocity in degrees per second
 float positionErrorSum = 0.0; // Position error integral sum
 float velocityErrorSum = 0.0; // Velocity error integral sum
 
-int middlePWM = 127; // in the case of 8 bit PWM
+int middlePWM = 512; // in the case of 10 bit PWM
 
 // Velocity integral gain
 
@@ -86,12 +97,11 @@ volatile bool shouldReadAngle = true;
 float angleDiff;
 float angleDiffRad;
 
-const unsigned long sampleTime = 10;  // Sample time in milliseconds
+const unsigned long sampleTime = 10; // Sample time in milliseconds
 
-
-double Kp = 3.5;   // Proportional gain
+double Kp = 3.5;  // Proportional gain
 double Ki = 0.25; // Integral gain
-double Kd = 0.0; // Derivative gain
+double Kd = 0.0;  // Derivative gain
 
 PID pid(&input, &output, &setpoint, Kp, Ki, Kd, DIRECT);
 
@@ -138,14 +148,25 @@ void IRAM_ATTR onTimer()
   shouldReadAngle = true;
 }
 
+ 
+
 void setup()
 {
-  ledcSetup(0, 20000, 8); // Channel 0, 10 kHz frequency, 8-bit resolution
-  ledcSetup(1, 20000, 8); // Channel 1, 10 kHz frequency, 8-bit resolution
-  ledcSetup(2, 20000, 8); // Channel 2, 10 kHz frequency, 8-bit resolution
-  ledcSetup(3, 20000, 8); // Channel 3, 10 kHz frequency, 8-bit resolution
-  ledcSetup(4, 20000, 8); // Channel 4, 10 kHz frequency, 8-bit resolution
-  ledcSetup(5, 20000, 8); // Channel 5, 10 kHz frequency, 8-bit resolution
+
+
+ 
+  // BLDCMotor motor = BLDCMotor(21);
+
+
+
+
+
+  ledcSetup(0, 20000, 10); // Channel 0, 10 kHz frequency, 10-bit resolution
+  ledcSetup(1, 20000, 10); // Channel 1, 10 kHz frequency, 10-bit resolution
+  ledcSetup(2, 20000, 10); // Channel 2, 10 kHz frequency, 10-bit resolution
+  ledcSetup(3, 20000, 10); // Channel 3, 10 kHz frequency, 10-bit resolution
+  ledcSetup(4, 20000, 10); // Channel 4, 10 kHz frequency, 10-bit resolution
+  ledcSetup(5, 20000, 10); // Channel 5, 10 kHz frequency, 10-bit resolution
 
   // Attach the PWM channels to the motor phase pins
 
@@ -169,7 +190,7 @@ void setup()
   timerAttachInterrupt(timer, &onTimer, true); // Attach the interrupt handler
 
   // Compute the necessary values for a 100 Hz interrupt
-  float interruptFrequency = 20.0;          // Desired interrupt frequency in Hz
+  float interruptFrequency = 80.0;          // Desired interrupt frequency in Hz
   float timerFrequency = 40000000.0 / 80.0; // Timer frequency with prescaler
   float alarmValue = timerFrequency / interruptFrequency;
 
@@ -179,9 +200,7 @@ void setup()
 
   // currentPosition = smoothTransition(currentPosition, targetPosition);
 
-  pid.SetMode(AUTOMATIC);       // Set the PID mode to AUTOMATIC
-  pid.SetOutputLimits(-1023, 1023); // Set the output limits of the PID controller
-  pid.SetSampleTime(10);        // Set the sample time (in milliseconds)
+  
 }
 
 unsigned long previousTime = 0;
@@ -193,206 +212,147 @@ float offsetStep = 0.4;
 float kp_step = 0.1;
 float ki_step = 0.1;
 
+float currentPositionRad = 0;
+
 void loop()
 {
 
-  // if (Serial.available())
-  // {
-  //   String input_ = Serial.readStringUntil('\n');
-  //   // Serial.println("This is received from GUI: ");
-  //   // Serial.println(input);
-  //   parseSerialInput(input_);
-
-  //   if (recevedCommand != "")
-  //   {
-  //     Serial.println("Response from DRV8301: ");
-  //     Serial.println(readWriteRegister(recevedCommand));
-  //   }
-
-  //   // Process the received command
-  //   if (input_ == "K")
-  //   {
-
-  //     Kp -= kp_step;
-
-  //     // dtostrf(OFFSET, 6, 3, buffer); // 6 total characters, including 3 decimal places
-  //     Serial.println("KP: ");
-  //     Serial.print(Kp);
-  //   }
-  //   else if (input_ == "L")
-  //   {
-
-  //     Kp += kp_step;
-  //     Serial.println("KP: ");
-  //     Serial.print(Kp);
-  //   }
-
-  //   if (input_ == "O")
-  //   {
-
-  //     Serial.println("KI ");
-  //     Serial.print(Ki);
-  //   }
-  //   else if (input_ == "P")
-  //   {
-
-  //     Serial.println("KI ");
-  //     Serial.print(Ki);
-  //   }
-  // }
 
 
-  unsigned long currentTime = millis();
-  unsigned long elapsedTime = currentTime - previousTime;
+  
 
+  // Wrap the position within the valid range [0, MAX_POSITION]
+ 
 
-  if (elapsedTime >= sampleTime) {
+  if (Serial.available())
+  {
+    String input_ = Serial.readStringUntil('\n');
+    // Serial.println("This is received from GUI: ");
+    // Serial.println(input);
+    parseSerialInput(input_);
+
+    if (recevedCommand != "")
+    {
+      Serial.println("Response from DRV8301: ");
+      Serial.println(readWriteRegister(recevedCommand));
+    }
+
+    // Process the received command
+    if (input_ == "K")
+    {
+
+      OFFSET -= 0.1;
+
+      // dtostrf(OFFSET, 6, 3, buffer); // 6 total characters, including 3 decimal places
+      Serial.println("OFFSET: ");
+      Serial.print(OFFSET);
+    }
+    else if (input_ == "L")
+    {
+
+      OFFSET += 0.1;
+      Serial.println("OFFFSET: ");
+      Serial.print(OFFSET);
+    }
+
+    if (input_ == "O")
+    {
+
+      Serial.println("KI ");
+      Serial.print(Ki);
+    }
+    else if (input_ == "P")
+    {
+
+      Serial.println("KI ");
+      Serial.print(Ki);
+    }
+  }
+
+  // unsigned long currentTime = millis();
+  // unsigned long elapsedTime = currentTime - previousTime;
+
+  // // if (elapsedTime >= sampleTime)
+  // // {
 
   input = (double)readAngle1(csPin);
 
-
-
   double currentPositionRad = input * PI / 180;
 
-  setpoint = (double)targetPosition * PI/180;
-
-  float error = smoothTransition(currentPositionRad, setpoint);
-
-  integralSum += error * elapsedTime/1000.0;
-
-
-  
-  integralSum = constrain(integralSum, -integralWindupMax, integralWindupMax);
-
-
-  output = Kp * error + Ki * integralSum;
-
-  // input = (double)error;
-
-  // setpoint = 0.0;
-
-  // pid.Compute();
-
-
-
-  // float pidOutput = Kp*error ;
-  float constrainedOutput = mapFloat(output, -10, 10, -1.0, 1.0);
-
-  Serial.println("Error:");
-  Serial.println(error);
-
-  Serial.println("Setpoint:");
-  Serial.println(setpoint);
-
-  Serial.println(" Output:");
-  Serial.println(output);
-
-  Serial.println("Constrained Output:");
-  Serial.println(constrainedOutput);
-  delay(20);
-
-  
-
-
-  //   if (shouldReadAngle)
-  //   {
-
-  //     currentTime = micros();
-
-  //     currentPosition = readAngle1(csPin);
-
-  //     // Serial.println("currentPosition");
-  //     // Serial.println(currentPosition);
-
-  //     float currentPositionSmooth = smoothTransition(currentPosition, targetPosition);
-
-  //     Serial.println("currentPosition ");
-  //     Serial.println(currentPositionSmooth);
-
-  //     positionErrorSum += angleDiffRad ;
-
-  //     if (positionErrorSum >= 100){
-
-  //       positionErrorSum = 100;
-  //     }
-  //     else if ( positionErrorSum <-100){
-
-  //       positionErrorSum = -100;
-
-  //     }
-
-  //     float positionControl1 = -Kp * angleDiffRad - Ki * positionErrorSum ;
-
-  //      Serial.println("Control");
-  //     Serial.println(positionControl1,6);
-
-  //     float constrainedControl = constrain(positionControl1, -100, 100);
-
-  //     mappedControl = mapFloat(constrainedControl, -100, 100, -0.4, 0.4);
-
-  //     // Serial.println("Mapped control");
-  //     // Serial.println(mappedControl);
-
-  //     // Serial.println("Target angle");
-  //     // Serial.println(targetPosition);
-
-  //     controlMotor();
-  //     shouldReadAngle = false;
-  //   }
-  // }
-
-  // void controlMotor()
-  // {
-
-  //   dutyCycle = abs(mappedControl);
-
-  //   float currentPositionRad = currentPosition * PI / 180.0; // Convert to radians
-
-  //   // Serial.println("DutyCycle");
-
-  //   // Serial.println(dutyCycle);
-
-  float U = middlePWM + middlePWM * output * sin(currentPositionRad * POLE_PAIRS);
-
-  float V = middlePWM + middlePWM * output * sin(currentPositionRad * POLE_PAIRS + PHASE_DELAY_1);
-
-  float W = middlePWM + middlePWM * output * sin(currentPositionRad * POLE_PAIRS + PHASE_DELAY_2);
-
-  if (U < middlePWM)
+   if (currentPositionRad >= MAX_POSITION)
   {
+    currentPositionRad -= MAX_POSITION;
+  }
+
+
+  Serial.println(currentPositionRad);
+
+
+  currentPositionRad += 0.1; // JUST INCREMENT IT A BIT]
+  Serial.println(currentPositionRad);
+
+  // // setpoint = (double)targetPosition * PI / 180;
+
+  // // float error = smoothTransition(currentPositionRad, setpoint);
+
+  // // integralSum += error * elapsedTime / 1000.0; // /1000 to get per second
+
+  // // integralSum = constrain(integralSum, -integralWindupMax, integralWindupMax);
+
+  // // output = Kp * error + Ki * integralSum;
+
+  // // float constrainedOutput = mapFloat(output, -10, 10, -0.6, 0.6);
+
+  // // Serial.println("Error:");
+  // // Serial.println(error);
+
+  float U = middlePWM + middlePWM * 0.35 * sin(currentPositionRad + OFFSET);
+  float V = middlePWM + middlePWM * 0.35 * sin(currentPositionRad + 2.094395102 + OFFSET); // Phase shift of 120 degrees
+  float W = middlePWM + middlePWM * 0.35 * sin(currentPositionRad + 4.188790205 + OFFSET); // Phase shift of 240 degrees
+  // Serial.print(U, 2);
+  // Serial.print(",");
+  // Serial.print(V, 2);
+  // Serial.print(",");
+  // Serial.print(W, 2);
+  // Serial.println();
+
+ int middlePWM = 512; // Half of the PWM resolution (e.g., for 10-bit PWM: 1024 / 2 = 512)
+
+// Generate complementary PWM signals for Phase A
+ if (U < middlePWM)
+ {
     inL_A = int(U);
-    inH_A = 0;
-  }
-
-  if (U >= middlePWM)
-  {
-    inL_A = 0;
+    inH_A = 1023 - inL_A;
+ }
+ else
+ {
     inH_A = int(U);
-  }
+    inL_A = 1023 - inH_A;
+ }
 
-  if (V < middlePWM)
-  {
+ // Generate complementary PWM signals for Phase B
+ if (V < middlePWM)
+ {
     inL_B = int(V);
-    inH_B = 0;
-  }
-
-  if (V >= middlePWM)
-  {
-    inL_B = 0;
+    inH_B = 1023 - inL_B;
+ }
+ else
+ {
     inH_B = int(V);
-  }
+    inL_B = 1023 - inH_B;
+ }
 
-  if (W < middlePWM)
-  {
+ // Generate complementary PWM signals for Phase C
+ if (W < middlePWM)
+ {
     inL_C = int(W);
-    inH_C = 0;
-  }
-
-  if (W >= middlePWM)
-  {
-    inL_C = 0;
+    inH_C = 1023 - inL_C;
+ }
+ else
+ {
     inH_C = int(W);
-  }
+    inL_C = 1023 - inH_C;
+ }
 
   ledcWrite(0, int(inH_A)); // Write PWM value to Channel 0
   ledcWrite(1, int(inL_A)); // Write PWM value to Channel 1
@@ -403,7 +363,7 @@ void loop()
   ledcWrite(4, int(inH_C)); // Write PWM value to Channel 4
   ledcWrite(5, int(inL_C)); // Write PWM value to Channel 5
 
-  previousTime = currentTime;
+ 
 
-  }
-};
+  delay(50);
+}
